67a4705fc5e95ea2c087c8a1c854240c16a9eacf
Eric <rovx15@gmail.com>
2017-01-28 13:51:25 +0100

    Mas metodos hechos

--- src/exercici1/Exercici1.java
+++ src/exercici1/Exercici1.java
@@ -3,7 +3,17 @@ package exercici1;
 public class Exercici1 {
 
     public static void main(String[] args) {
-
+        Element e = new Element();
+        InfoElement info = new InfoElement(e);
+        
+        
+        System.out.println("1. nom de la superclasse: " + info.nomSuperClase());
+        System.out.println("2. nom de la classe complet: " + info.nomClaseComplet());
+        System.out.println("3. només el nom de la classe: " + info.nomesNomClase());
+        System.out.println("4. array d'atributs (Field): " + info.arrayAtributs());
+        System.out.println("5. array de mètodes (Method): " + info.arrayMetodes());
+        System.out.println("6. array dels noms dels atributs: " + info.arrayNomAtributs());
+        System.out.println("7. array dels noms dels mètodes: " + info.nomMetodes());
     }
     
 }
--- src/exercici1/InfoElement.java
+++ src/exercici1/InfoElement.java
@@ -8,57 +8,82 @@ import java.lang.reflect.Method;
  * @author Eric
  */
 public class InfoElement {
-    
+
     private Element e;
 
     public InfoElement(Element e) {
         this.e = e;
     }
-    
-    public String nomSuperClase(){
+
+    public String nomSuperClase() {
         return e.getClass().getSuperclass().getName();
     }
-    
-    public String nomClaseComplet(){
+
+    public String nomClaseComplet() {
         return e.getClass().getCanonicalName();
     }
-    
-    public String nomesNomClase(){
+
+    public String nomesNomClase() {
         return e.getClass().getSimpleName();
     }
-    
-    public Field[] arrayAtributs(){
+
+    public Field[] arrayAtributs() {
         return e.getClass().getDeclaredFields();
     }
-    
-    public Method[] arrayMetodes(){
+
+    public Method[] arrayMetodes() {
         return e.getClass().getDeclaredMethods();
     }
-    
-    public String[] arrayNomAtributs(){
+
+    public String[] arrayNomAtributs() {
         String[] nomAtributs = new String[arrayAtributs().length];
         Field[] att = arrayAtributs();
-        for(int i = 0; i < nomAtributs.length; i++){
+        for (int i = 0; i < nomAtributs.length; i++) {
             nomAtributs[i] = att[i].getName();
-        }        
+        }
         return nomAtributs;
     }
-    
-    public String[] nomMetodes(){
+
+    public String[] nomMetodes() {
         String[] nomMetodes = new String[arrayMetodes().length];
         Method[] meth = arrayMetodes();
         for (int i = 0; i < nomMetodes.length; i++) {
             nomMetodes[i] = meth[i].getName();
         }
         return nomMetodes;
-    }   
-    
-    public int[] modAtribut(){
-        int[] nomMetodes = new int[arrayMetodes().length];
+    }
+
+    public int[] modAtribut() {
+        int[] modAtribut = new int[arrayMetodes().length];
         Method[] meth = arrayMetodes();
-        for (int i = 0; i < nomMetodes.length; i++) {
-            nomMetodes[i] = meth[i].getModifiers();
+        for (int i = 0; i < modAtribut.length; i++) {
+            modAtribut[i] = meth[i].getModifiers();
         }
-        return nomMetodes;
-    }    
+        return modAtribut;
+    }
+
+    public int[] modMetode() {
+        int[] modModificador = new int[arrayAtributs().length];
+        Field[] field = arrayAtributs();
+        for (int i = 0; i < modModificador.length; i++) {
+            modModificador[i] = field[i].getModifiers();
+        }
+        return modModificador;
+    }
+
+    public void modificadorValAtr() {
+        //TODO
+    }
+    
+    public void executarMetode(){
+        e.getCad(); //???
+    }
+    
+    //TODO crear Instancia
+
+    @Override
+    public Element clone() throws CloneNotSupportedException {
+        return new Element(e.getNum(), e.getCad());
+    }
+
 }
